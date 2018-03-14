72d8bf733d08b254841dd7ffe1c5c2a6fbc9de05
Brodovych <iisbrody@gmail.com>
2018-03-14 14:55:46 +0200

    Worked on Main Javascript

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+
+// Save Bookmark
+function saveBookmark(e){
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    
+    /*
+    // Local Storage Test
+        localStorage.setItem('test', 'Hello World');
+        console.log(localStorage.getItem('test'));
+        localStorage.removeItem('test');
+        console.log(localStorage.getItem('test'));
+    */
+
+    // Prevent form from submitting
+    e.preventDefault();
+}
